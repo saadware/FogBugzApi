98765957c44cf964e7499e64f1931b21b4a43cae
Scott Saad <saadsj@gmail.com>
2013-09-09 18:55:44 -0600

    Updated AssemblyInfo
    Updated the assembly info file to include some good details. Prepping
    for nuget.

--- FogBugzApi/Properties/AssemblyInfo.cs
+++ FogBugzApi/Properties/AssemblyInfo.cs
@@ -6,13 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FogBugzApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("LINQ friendly wrapper for consuming the FogBugz API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Scott Saad")]
 [assembly: AssemblyProduct("FogBugzApi")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
